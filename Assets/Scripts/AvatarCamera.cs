de60d11471ccfa1cc699c9b3dbf1cca86b1f9023
Dusty Monk <dustym.windstorm@gmail.com>
2015-10-03 15:52:38 -0500

    Initial add of project
    Adding the Unity Project files to the depot.

---
+++ Assets/Scripts/AvatarCamera.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarCamera : MonoBehaviour
+{
+
+    public Transform TargetObj;
+    public float LookSmoothFactor = 0.09f;
+    public Vector3 TargetOffset = new Vector3(0, -6, -8);
+    public float CameraTilt = 10;
+
+    Vector3 Destination = Vector3.zero;
+    AvatarController TargetController;
+    float TurnSpeed = 0.0f;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Init Controller..
+        TargetController = TargetObj.GetComponent<AvatarController>();
+        if (TargetController == null)
+        {
+            Debug.LogError("Target Object does not contain an avatar controller.");
+            return;
+        }
+	}
+	
+	// Update is called once per frame
+    /// <summary>
+    /// This happens after Update & FixedUdates, so we update our position here, to make sure we're using the most recent information
+    /// for the controller, based on this frame
+    /// </summary>
+	void LateUpdate ()
+    {
+        MoveToTarget();
+        LookAtTarget();
+	}
+
+    void MoveToTarget()
+    {
+        // Rotated Point
+        Destination = TargetController.DesiredRotation * TargetOffset;
+        Destination += TargetObj.position;
+        transform.position = Destination;
+    }
+
+    void LookAtTarget()
+    {
+        // Smooth Damp Angle will peform a nicely smoothed interpreted turn from source to target, returning a rotational speed, given a time to turn by.
+        float eulerAngleY = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetObj.eulerAngles.y, ref TurnSpeed, LookSmoothFactor);
+        transform.rotation = Quaternion.Euler(TargetObj.eulerAngles.x + (CameraTilt * Mathf.Deg2Rad), eulerAngleY, 0);
+        
+
+    }
+
+}
---
+++ Assets/Scripts/AvatarController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+// [RequireComponent(typeof(CapsuleCollider))]
+
+public class AvatarController: MonoBehaviour
+{
+    public float ForwardSpeed = 10.0f;
+    public float TurnSpeed = 100;
+
+    float inputDelay = 0.1f;
+    Rigidbody rigidBody;
+    CapsuleCollider capsuleCollider;
+    Animator animator;
+    float turnAmount;
+    float forwardAmount;
+    Quaternion desiredRotation;
+
+    public Quaternion DesiredRotation
+    {
+        get { return desiredRotation;  }
+    }
+
+    // Input Amounts - Eventually these will come from a separate controller
+    float forwardInput = 0.0f;
+    float turnInput = 0.0f;
+
+
+    /// <summary>
+    /// Property Initializaion
+    /// </summary>
+    void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
+        animator = GetComponent<Animator>();
+        desiredRotation = transform.rotation;
+        forwardInput = 0.0f;
+        turnInput = 0.0f;
+	}
+
+    /// <summary>
+    /// Very simple GetInput Function. Eventually, we'll have a separate input controller to handle user
+    /// input. But to get this up and running, we'll just grab current inputs..
+    /// </summary>
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    /// <summary>
+    /// Non physics Updates
+    /// </summary>
+    void Update ()
+    {
+        GetInput();
+        DoTurn();
+	}
+
+    /// <summary>
+    /// Physics Updates get updated here
+    /// </summary>
+    void FixedUpdate()
+    {
+        DoMovement();
+    }
+
+    /// <summary>
+    /// Calculate Forward Movement Velocity
+    /// </summary>
+    void DoMovement()
+    {
+        // Only start to move when we've exceeded the input threshold..
+        if (Mathf.Abs(forwardInput) < inputDelay)
+        {
+            rigidBody.velocity = Vector3.zero;
+            animator.SetFloat("ForwardSpeed", 0.0f);
+            return;
+        }
+
+        // Clamp Backwards Movement to only be half of max forward movement..
+        if (forwardInput < 0.0f)
+            forwardInput = Mathf.Max(-0.5f, forwardInput);
+
+        // Adjust forward velocity by runspeed * input..
+        forwardAmount = forwardInput * ForwardSpeed;
+        animator.SetFloat("ForwardSpeed", forwardAmount);
+        rigidBody.velocity = transform.forward * forwardAmount;
+
+    }
+
+    void DoTurn()
+    {
+        if (Mathf.Abs(turnInput) >= inputDelay)
+        {
+            desiredRotation *= Quaternion.AngleAxis(TurnSpeed * turnInput * Time.deltaTime, Vector3.up);
+            rigidBody.rotation = desiredRotation;
+        }
+    }
+
+
+
+}
